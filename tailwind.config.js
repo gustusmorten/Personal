b11dc5666172d5f46a6b036c2ebc4ad11d316b3c
Peps <jose.camacho@konfio.mx>
2023-05-29 17:57:16 -0600

    migrate to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'sky': '#0ea5e9',
+      },
+      padding: {
+        'card': '4.5em 3em 3em 3em',
+      },
+      width: {
+        'card': '27em',
+      },
+    },
+  },
+  plugins: [],
+}
